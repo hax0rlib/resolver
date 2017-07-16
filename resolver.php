91aa6bfcc381f3e453778a875cec3390a219a4f8
Hαx0rℓiв - Dєvєℓσρєr <andretutors0@gmail.com>
2017-07-16 18:21:41 -0300

    Update POO

--- resolver.php
+++ resolver.php
@@ -1,41 +1,77 @@
 <?php
 
-$skypeAccount = 'SKYPE_ACCOUNT';
-$key = 'YOUR_API_KEY';
-$url = "http://api.skresolver.com/?format=json&apikey=$key&user=$skypeAccount"; //For multiple add: &multiple=1
+/***********************************************************************************************************************
+*    _ (`-. ('-. .-.  ('-.        .-') _ .-') _     ('-.     .-')  _   .-')              ('-.   .-. .-')
+*  ( (OO  ( OO )  / ( OO ).-.   ( OO ) (  OO) )   ( OO ).-.( OO )( '.( OO )_           ( OO ).-\  ( OO )
+*  _.`     ,--. ,--. / . --. ,--./ ,--,'/     '._  / . --. (_)---\_,--.   ,--.,--.      / . --. /;-----.\
+* (__...--'|  | |  | | \-.  \|   \ |  |\|'--...__) | \-.  \/    _ ||   `.'   ||  |.-')  | \-.  \ | .-.  |
+* |  /  | |   .|  .-'-'  |  |    \|  | '--.  .--.-'-'  |  \  :` `.|         ||  | OO .-'-'  |  || '-' /_)
+* |  |_.' |       |\| |_.'  |  .     |/   |  |   \| |_.'  |'..`''.|  |'.'|  ||  |`-' |\| |_.'  || .-. `.
+* |  .___.|  .-.  | |  .-.  |  |\    |    |  |    |  .-.  .-._)   |  |   |  (|  '---.' |  .-.  || |  \  |
+* |  |    |  | |  | |  | |  |  | \   |    |  |    |  | |  \       |  |   |  ||      |  |  | |  || '--'  /
+* `--'    `--' `--' `--' `--`--'  `--'    `--'    `--' `--'`-----'`--'   `--'`------'  `--' `--'`------'
+************************************************************************************************************************
+ * @author @Hax0lirb
+ * @improvements Hax0rlib implement POO <3
+ * @version 2.0
+ * @since 16/07/2017
+ * @contact Telegram @hax0rlib @phantasmlab
+ *************************/
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-$result = curl_exec($ch);
-curl_close($ch); 
-$result = json_decode($result);
+class Resolver {
 
-$statusCode = $result->meta->statusCode;
-$data = $result->data;
-$user = $result->data->user;
-$ip = $result->data->ip;
-$port = $result->data->port;
-$country = $result->data->country;
-$avatarURL = $result->data->avatarURL;
+    public function infoAccount() {
 
-if($statusCode == 200)
-{
-	if(isset($data))
-	{
-		echo "User: $user <br />";
-		echo "IP: $ip <br />";
-		echo "Port: $port <br />";
-		echo "Country: $country <br />";
-		echo "avatar: $avatarURL";
+        $skypeAccount = 'SKYPE ACCOUNT'; // SEU ID DE LOGIN NO SKYPE
+        $key = 'YOUR_API_KEY'; // SUA CHAVE DA API
+        $url = 'http://api.skresolver.com/?format=json&apikey=$key&user=$skypeAccount' // PARA MULTIPLAS CONTAS ADD: &multiple=1
 	}
-	else
-		echo "No IP address has been found";
-}
-else
-{
-	echo "Service unavailable";
+
+    public function sessioncURL() {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, infoAccount()->$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($result);
+        $statusCode = $result->meta->statusCode;
+        $data = $result->data;
+        $user = $result->data->user;
+        $ip = $result->data->ip;
+        $port = $result->data->port;
+        $country = $result->data->country;
+        $avatarURL = $result->data->avatarURL;
+    }
+
 }
 
-?>
+    public function printData() {
+
+        if(isset($data))
+        {
+            echo "User: sessioncURL()->$user <br />";
+            echo "IP: sessioncURL()->$ip <br />";
+            echo "Port: sessioncURL()->$port <br />";
+            echo "Country: sessioncURL()->$country <br />";
+            echo "avatar: sessioncURL()->$avatarURL";
+        }
+        else
+            echo "No IP address has been found";
+    }
+    else
+    {
+        echo "Service unavailable";
+    }
+}
+
+
+/**********************************
+ *  MODO DE USAR
+ *  BY HAX0RLIB
+ *  EX:
+ * $resolver = new Resolver();
+ * $resolver->printData();
+ **********************************/
+
